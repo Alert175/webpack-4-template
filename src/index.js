14e5ae2b9c923eef240a458784910a788832c3a4
Alert175 <40724267+Alert175@users.noreply.github.com>
2019-04-22 19:01:53 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-let hello = (first, second) => first + second;
 
-console.log(hello('ilya ', 'gusev'));
+// JS - ./js/index.js
+import './js/'
+// CSS (example)
+import './css/main.css'
+
+// Bootstrap (example)
+// import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
